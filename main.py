1833b2ef73fe5d223ce234d54e61f362444a9dd3
zucchi99 <francesco.zuccato99@gmail.com>
2024-12-27 10:14:48 +0000

    test

---
+++ main.py
@@ -0,0 +1,199 @@
+
+import sys
+sys.path.insert(0, 'src')
+from functions import *
+
+params = DEFAULT_PARAMETERS
+
+notebooks = [
+    '0_extend__df_raw', 
+    '1_explore__df_ext', 
+    '2_estimate_factors', 
+    ###'3_add_concentrations', 
+    '4_simulate_flux_ARIMA', 
+    '5a_add_initial_flux', 
+    '5b_estimate_flux_slope', 
+    '5c_estimate_flux_min', 
+    '6_estimate_flux',
+    '7_load_uppaal_series',
+    '8_compare_estimated_vs_uppaal',
+]
+
+input_data_path = {
+    '0_extend__df_raw' :                PATH_SENSORS_DATA_RAW_UF,
+    '1_explore__df_ext' :               PATH_SENSORS_DATA_EXT_UF_V1,
+    '2_estimate_factors' :              PATH_SENSORS_DATA_EXT_UF_V1,
+    '4_simulate_flux_ARIMA':            PATH_SENSORS_DATA_EXT_UF_V1,
+    ###'3_add_concentrations':          PATH_SENSORS_DATA_EXT_UF_V2,
+    '5a_add_initial_flux':              PATH_SENSORS_DATA_EXT_UF_V3,
+    '5b_estimate_flux_slope':           PATH_SENSORS_DATA_EXT_UF_V3,
+    '5c_estimate_flux_min':             PATH_SENSORS_DATA_EXT_UF_V3,
+    '6_estimate_flux':                  PATH_SENSORS_DATA_EXT_UF_V3,
+    '7_load_uppaal_series':             PATH_UPPAAL_DATA_RAW,
+    '8_compare_estimated_vs_uppaal':    PATH_UPPAAL_DATA_EXT,
+}
+
+def merge_data_files() :
+    df_all = pd.DataFrame()
+    i = 0
+    output_file = 'ALL_DATA.csv'
+    output_file_path = f"{PATH_SENSORS_DATA_EXT_UF_V1}/{output_file}"
+    for f in sorted(os.listdir(PATH_SENSORS_DATA_EXT_UF_V1)) :
+        if re.match('.*\.csv', f) and (not f in [output_file]) : 
+            # read file
+            f = PATH_SENSORS_DATA_EXT_UF_V1 + '/' + f
+            df_cur = pd.read_csv(f)
+            # drop outliers
+            if drop_initial_final_off_rows :
+                df_cur = drop_initial_final_rows(df_cur, log=False)
+                df_cur, _ = get_df_ON_OFF(df_cur)
+            if drop_off_rows :
+                args = { 'log' : False }
+                if drop_outliers :
+                    df_cur, _ = remove_outliers(df_cur, cols=['res tot [1/m]'],    drop_fun=drop_outliers_far_median, args=args)
+                    df_cur, _ = remove_outliers(df_cur, cols=['prs feed_2 [kPa]'], drop_fun=drop_outliers_far_neighbours, args=args)
+                    df_cur, _ = remove_outliers(df_cur, cols=['flux [L/m^2h]'],    drop_fun=drop_outliers_out_range, args=args)
+                    df_cur, _ = remove_outliers(df_cur, cols=['flux [L/m^2h]'],    drop_fun=drop_initial_jumps, args=args)
+            # concatenate
+            df_cur['file_idx'] = i
+            #df_cur.loc[len(df_cur)] = empty_separator_row
+            df_all = pd.concat([df_all, df_cur])
+            i += 1
+    df_all = df_all.drop('index', axis=1).reset_index(drop=True)
+    # update tmp groups
+    new_group = 0
+    df_all.loc[0, 'new group'] = new_group
+    for i in range(1, len(df_all)) :
+        file_cur = df_all.loc[i, 'file_idx']
+        file_prv = df_all.loc[i-1, 'file_idx']
+        group_cur = df_all.loc[i, 'TMP group']
+        group_prv = df_all.loc[i-1, 'TMP group']
+        new_group += (1 if ((file_cur != file_prv) or (group_cur != group_prv)) else 0)
+        df_all.loc[i, 'new group'] = new_group
+        #print(f'files: {file_prv} -> {file_cur}, group: {group_prv} -> {group_cur}, new_group:{new_group}')
+    df_all = df_all.drop('TMP group', axis=1).rename(columns={'new group' : 'TMP group'})
+    # write to file
+    df_all.to_csv(output_file_path, index=False)
+
+def update_parameters_json(args=DEFAULT_PARAMETERS) :
+    with open(FILE_PARAMETERS, 'w') as fid:
+        json.dump(args, fid, indent=2)
+
+def init_chars(n, char='#') :
+    return ''.join([char for _ in range(n)])
+
+def run_all() : 
+    j = 0
+    for notebook in notebooks :
+        f_ipynb = f'src/{notebook}.ipynb'
+        n_chars = 1
+        print("\n###################################################")
+        print(f"{init_chars(n_chars)} STEP: {j}  <=  <notebook_idx>")
+        n_chars = 2
+        print(f"{init_chars(n_chars)} notebook: '{f_ipynb}'")
+        data_path = input_data_path.get(notebook, None)
+        file_idx        = -1
+        file_idx_uppaal = -1
+        path_html = f'{PATH_NOTEBOOK_OUTPUT}{notebook}'
+        os.makedirs(path_html, exist_ok=True)
+        # one execution per file        # one execution per input file
+        # input files are ordered by date (alphabetically order with file names 'yyyy-mm-dd .*')
+        # last input file may be ALL_DATA.csv
+        in_files = sorted(os.listdir(data_path))
+        for f_input in in_files :
+            file_idx        += 1
+            file_idx_uppaal += 1
+            f_html = f'{path_html}/{f_input}-{file_idx}.html'
+            n_chars = 3
+            print(f"{init_chars(n_chars)} STEP: {j}.{file_idx}  <=  <notebook_idx>.<concentration_idx>")
+            n_chars = 4
+            #print(f"{init_chars(n_chars)} input data path: '{data_path}'")
+            print(f"{init_chars(n_chars)} input data file: '{f_input}'")
+            print(f"{init_chars(n_chars)} output notebook file: '{f_html}'")
+            params['file_idx']        = file_idx
+            params['file_idx_uppaal'] = file_idx_uppaal
+            if notebook == '8_compare_estimated_vs_uppaal' :
+                if read_from_json(FILE_DATA_SIMULATIONS_ASSOC, f_input) == None :
+                    print(f"{init_chars(n_chars)} WARNING: for UPPAAL's simulation file '{f_input}' missing the associated real-data file, add association in file '{FILE_DATA_SIMULATIONS_ASSOC}'")
+                    continue
+            # one execution per file and per tmp
+            if notebook in ['4_simulate_flux_ARIMA'] :
+                if f_input != 'ALL_DATA.csv' :
+                    tmps_idxs = TMP_INTERVALS[f_input]
+                    k = 0
+                    for (tmp_idx, cur_idxs) in tmps_idxs.items() :
+                        f_html_2 = re.sub("\.html$", f"-{k}.html", f_html)
+                        n_chars = 5
+                        print(f"{init_chars(n_chars)} STEP: {j}.{file_idx}.{k}  <=  <notebook_idx>.<concentration_idx>.<transembrane_idx>")
+                        n_chars = 6
+                        print(f"{init_chars(n_chars)} current data rows: '{cur_idxs}'")
+                        #print(f"{init_chars(n_chars)} input data file: {f_input}")
+                        print(f"{init_chars(n_chars)} output notebook file: '{f_html_2}'")
+                        params['tmp_idx'] = tmp_idx
+                        update_parameters_json(params)
+                        run_notebook(f_ipynb, f_html_2)
+                        k += 1
+            else :
+                update_parameters_json(params)
+                run_notebook(f_ipynb, f_html)
+        if notebook == '0_extend__df_raw' :
+            merge_data_files()
+        j += 1
+
+def run_notebook(notebook_file, output_file):
+    """Pass arguments to a Jupyter notebook, run it and convert to html."""
+    # Run the notebook
+    subprocess.call([
+        'jupyter-nbconvert',
+        '--log-level=ERROR', # silent mode
+        '--execute',
+        '--to', 'html',
+        '--output', output_file,
+        notebook_file
+    ])
+
+
+def read_from_json(json_file, key) : 
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    return data.get(key, None)
+
+
+def create_directories() :
+    for dir_path in ALL_PATHS :
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+def clear_all_output_directories() : 
+    # recursive delete!!!
+    for dir_path in NON_RAW_PATHS :
+        shutil.rmtree(dir_path, ignore_errors=True)
+
+#############################
+# MAIN
+
+# delete all output directories
+# clear_all_output_directories()
+
+# create any missing directory
+create_directories()
+
+# reset parameters with default values
+update_parameters_json()
+
+# read parameters
+params = read_parameters()
+
+# set local parameters
+drop_outliers = params['drop_outliers']
+drop_initial_final_off_rows = params['drop_initial_final_off_rows']
+drop_off_rows = params['drop_off_rows']
+
+# set notebook global parameters
+params['plot_scatterplot_matrix'] = False
+
+# run pipeline
+run_all()
+
+# reset parameters with default values
+update_parameters_json()
---
+++ src/constants.py
@@ -0,0 +1,268 @@
+############################# PACKAGES
+import pandas as pd
+import os
+import re
+import sys
+import argparse
+import subprocess
+import math
+import matplotlib
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as plot
+import matplotlib.dates as mdates
+import scipy.interpolate as interp
+import numpy as np
+import statsmodels.api as sm
+import statsmodels.graphics.tsaplots as tsaplots
+import statsmodels.tsa.stattools as tsatools
+import statsmodels.stats.outliers_influence as outliers_influence
+import statsmodels.tsa.arima.model as arima
+import pmdarima as pm
+import scipy.stats
+import statistics as stats
+import seaborn as sn
+import sklearn as sk
+import sklearn.linear_model as sk_lm
+import sklearn.metrics as metrics
+import datetime
+import json
+sys.path.append(r"PathToTheCoolPropFolder")
+import CoolProp.CoolProp as CP
+import shutil
+
+############################# DIRECTORIES
+# MAIN DIRECTORIES
+PATH_MAIN   = '/app/'
+PATH_DATA   = PATH_MAIN + 'data/'
+PATH_OUT    = PATH_MAIN + 'output/'
+
+# INPUT DIRECTORIES
+PATH_CONCENTRATIONS              = PATH_DATA + 'concentrations/'
+
+PATH_SENSORS_DATA_RAW_UF         = PATH_DATA + 'from_sensors/0_raw/UF/'
+PATH_SENSORS_DATA_EXT_UF_V1      = PATH_DATA + 'from_sensors/1_extended/UF/v1/'
+PATH_SENSORS_DATA_EXT_UF_V2      = PATH_DATA + 'from_sensors/1_extended/UF/v2/'
+PATH_SENSORS_DATA_EXT_UF_V3      = PATH_DATA + 'from_sensors/1_extended/UF/v3/'
+
+PATH_UPPAAL_DATA_RAW             = PATH_DATA + 'from_uppaal/0_raw/'
+PATH_UPPAAL_DATA_RAW_REPAIRED    = PATH_DATA + 'from_uppaal/1_repaired/'
+PATH_UPPAAL_DATA_EXT             = PATH_DATA + 'from_uppaal/2_extended/'
+
+# OUTPUT DIRECTORIES
+PATH_IMAGES                      = PATH_OUT + 'images/'
+PATH_NOTEBOOK_OUTPUT             = PATH_OUT + 'notebooks_html/'
+PATH_ESTIMATED_COEFFICIENTS      = PATH_OUT + 'estimated_coefficients/'
+
+############################# FILES
+# PARAMETERS FILE
+FILE_PARAMETERS                  = PATH_MAIN + 'parameters.json'
+FILE_DATA_SIMULATIONS_ASSOC      = PATH_MAIN + 'real_data__uppaal_simulations__association.json'
+
+# INPUT FILES
+FILE_CONCENTRATIONS_DATA         = PATH_CONCENTRATIONS + 'concentrations.xlsx'
+
+# OUTPUT FILES (stored in PATH_OUT directory)
+FILE_EST_COEFFS                  = PATH_ESTIMATED_COEFFICIENTS + 'estimated_coefficients.json'
+
+# OUTPUT DATA (stored in PATH_DATA directory)
+FILE_SENSORS_DATA_EST_PARAMS     = PATH_SENSORS_DATA_EXT_UF_V3 + '/estimated_parameters.csv'
+
+ALL_PATHS =  [
+    # MAIN DIRECTORIES
+    PATH_DATA,
+    PATH_OUT,
+    # INPUT DIRECTORIES
+    PATH_CONCENTRATIONS,
+    PATH_SENSORS_DATA_RAW_UF,
+    PATH_SENSORS_DATA_EXT_UF_V1,
+    PATH_SENSORS_DATA_EXT_UF_V2,
+    PATH_SENSORS_DATA_EXT_UF_V3,
+    PATH_UPPAAL_DATA_RAW,
+    PATH_UPPAAL_DATA_RAW_REPAIRED,
+    PATH_UPPAAL_DATA_EXT,
+    # OUTPUT DIRECTORIES
+    PATH_IMAGES,
+    PATH_NOTEBOOK_OUTPUT,
+    PATH_ESTIMATED_COEFFICIENTS
+]
+
+# NEVER DELETE THESE!!!
+RAW_DATA_PATHS = [
+    PATH_DATA,
+    PATH_CONCENTRATIONS,
+    PATH_SENSORS_DATA_RAW_UF,
+    PATH_UPPAAL_DATA_RAW,
+]
+
+# CAN BE DELETED WITHOUT ANY DAMAGE (BUT OUTPUT IS LOST)
+NON_RAW_PATHS = list(set(ALL_PATHS) - set(RAW_DATA_PATHS))
+
+############################# CONSTANTS
+
+x_axis   = ('time [m]', 'datetime', 'index')
+x_format = (None,       '%H:%M',    None)
+TIME_MINS = 0
+DATE_TIME = 1
+ROW_IDX_AS_TIME = 2
+
+# by default: only one global
+DEFAULT_CONC_GROUP = {
+    0 : ((None, None), 'unknown')   
+}
+
+# key file names are the one in folder PATH_SENSORS_DATA_RAW_UF
+CONCENTRATION_INTERVALS = {
+    "2023-11-08 clean water.csv" : {
+        0 : ((None, None), 'clean water')
+    },
+    "2023-11-09 clean + dirty water.csv" : {
+        0 : ((  6,   20), 'clean water'),
+        1 : (( 20,   79), 'dirty water'),
+        2 : (( 79,  106), 'dirty water'),
+        3 : ((154, None), 'dirty water')
+    }
+}
+
+# key file names are the one in folder PATH_SENSORS_DATA_EXT_UF_V1
+TMP_INTERVALS = {
+    "2023-11-08 0 clean water.csv" : {
+        0 : (None,  68),
+        1 : ( 68,  81),
+        2 : ( 81, 120),
+        3 : (120, 153),
+        4 : (153, 181),
+        5 : (181, 212),
+        6 : (212, None),
+    },
+    "2023-11-09 0 clean water.csv" : {
+        0 : (None, None) #(2, 21)
+    },
+    "2023-11-09 1 dirty water.csv" : {
+        0 : ( 1,  7),
+        #1 : (13,  17), # too few data
+        2 : (17, 22),
+        3 : (22, 28),
+        4 : (28, 36),
+        5 : (36, 44),
+        6 : (44, None),
+    },
+    "2023-11-09 2 dirty water.csv" : {
+        0 : ( 0,  9),
+        #4 : (17, 21), # too few data
+        5 : (22, None),
+    },
+    "2023-11-09 3 dirty water.csv" : {
+        #0 : ( 1,  5), # too few data
+        #1 : ( 5, 9), # too few data
+        #2 : ( 9, 12), # too few data
+        #3 : (12, 15), # too few data
+        4 : (15, 27),
+        5 : (27, None),
+    }
+}
+
+DEFAULT_PARAMETERS = {
+    "subfolder" : "UF/",
+    "file_idx_uppaal" : 0,
+    "file_idx" : 0,
+    "tmp_idx" : 0,
+    "initial_feed_volume_L" : 100,
+    "reset_columns_when_OFF" : True,
+    "drop_initial_final_off_rows" : True,
+    "drop_off_rows" : True,
+    "drop_outliers" : True,
+    "plot_scatterplot_matrix" : False,
+    "use_default_arima_params": True,
+    "default_arima_params": (1,1,0),
+    "include_arima_simulations_in_analysis": True,
+}
+
+ALL_N       = [0,      1,    1.5, 2 ]
+#ALL_MAX_K_N = [0.0002, 0.05, 0.2, 1 ] # AVOID OUTLIERS
+ALL_MAX_K_N = [0.00005, 0.002, 0.2, 1 ] # AVOID OUTLIERS
+
+# n -> fouling method
+FOULING_NAME = {
+    0   : "Cake filtration",
+    1   : "Intermediate pore blocking",
+    1.5 : "Internal (or Standard) pore blocking",
+    2   : "Complete pore blocking",
+}
+
+COLOR_CYCLE = plot.rcParams['axes.prop_cycle'].by_key()['color']
+
+MEMBRANE_USED = {
+    '2023-04-18' : ('INSIDE CeRAM', 3),
+    '2023-04-28' : ('INSIDE CeRAM', 3),
+    '2023-06-14' : ('INSIDE CeRAM', 3),
+    '2023-06-15' : ('INSIDE CeRAM', 3),
+    '2023-06-19' : ('INSIDE CeRAM', 3),
+    '2023-06-21' : ('INSIDE CeRAM', 3),
+    '2023-06-22' : ('INSIDE CeRAM', 3),
+    '2023-06-30' : ('INSIDE CeRAM', 3),
+    '2023-11-03' : ('INSIDE CeRAM', 3),
+    '2023-11-08' : ('COM0251178',   3),
+    '2023-11-09' : ('COM0251178',   3),
+    '2023-11-10' : ('COM0251178',   3),
+    '2024-07-11' : ('COM0251178',   3),
+    '2024-07-12' : ('COM0251178',   3),
+    '2024-07-15' : ('COM0251178',   3),
+    '2024-07-16' : ('COM0251178',   3),
+}
+
+UF_COLUMNS = {
+    'Date'          : 'date',
+    'Time'          : 'time',
+    'Millisecond'   : 'millisecond [ms]',
+    'LT1[%]'        : 'tank liters [%]',
+    'PT1[bar]'      : 'prs feed_1 [bar]',
+    'PT2[Bar]'      : 'prs feed_2 [bar]',
+    'PT3[bar]'      : 'prs permeate [bar]',
+    'PT4[bar]'      : 'prs retentate [bar]',
+    'TMP[bar]'      : 'TMP [bar]',
+    'FIT1[m³/h]'    : 'flow feed [m^3/h]',
+    'FIT2[m³/h]'    : 'flow permeate [m^3/h]',
+    'FIT3[m³/h]'    : 'flow retentate [m^3/h]',
+    'TT1[°C]'       : 'temperature [°C]'
+}
+
+MEMBRANES = {
+    'COM0251178' : {
+        # https://liqtech.com/ceramic-membranes-dpf/silicon-carbide-ceramic-membrane/comem-ceramic-membrane/
+        'aim'       : 'UF',
+        'material'  : 'ceramic',
+        'producer'  : 'LiqTech',
+        'model'     : 'COM0251178',
+        'channels'  : 30,
+        'channel dim' : 0.003, # [m]
+        'diameter'  : 0.025, # [m]
+        'length'    : 1.178, # [m]
+        'area'      : 0.33,  # [m^2]
+        'pore size' : 60     # [nm]
+    },
+    'INSIDE CeRAM' : {
+        # https://www.tami-industries.com/en/produits/inside-ceram-4/
+        'aim'       : 'UF',
+        'material'  : 'ceramic',
+        'producer'  : 'TAMI',
+        'model'     : 'INSIDE CeRAM',
+        'channels'  : 19,
+        'diameter'  : 0.025, # [m]
+        'length'    : 1.178, # [m]
+        'area'      : 0.25,  # [m^2]
+        #'pore size' : ?     # [nm]
+    },
+}
+
+UNIT_MEASURES = {
+    'bar'   : ('kPa', (lambda x :  100 * x)),
+    'm^3/h' : ('L/h', (lambda x : 1000 * x))
+}
+
+FEED_TANK_CAPACITY_LITERS = 150
+
+PRS_ATM_kpa = 101.325
+
+# mark point as outlier and drop row if res < MIN_RES or res > MAX_RES
+MIN_RES = 1e12
+MAX_RES = 1e15
